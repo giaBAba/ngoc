ac8a28a642e9003adc1e6e8c460e118f0589b8ab
giaBAba <138145525+giaBAba@users.noreply.github.com>
2023-08-03 00:14:24 +0700

    Update verus11.sh

--- verus11.sh
+++ verus11.sh
@@ -1,5 +1,5 @@
 
-for i in $(seq 7); do echo -e "\e[1;3"$i"m [Warning! ]:AE Chú Ý Chạy Lệnh Hiện Y/N == bấm Y Hết Nha Không Sẽ Bị Lỗi .cảm Ơn Ae ✓ \e[1m"; done
+for i in $(seq 7);do echo -e "\e[1;3"$i"m [Warning! ]:AE Chú Ý Chạy Lệnh Hiện Y/N == bấm Y Hết Nha Không Sẽ Bị Lỗi .cảm Ơn Ae ✓ \e[1m";done
 
 
 
