9d22a89573db29b0c954c112f6355e79a6034a4e
giaBAba <138145525+giaBAba@users.noreply.github.com>
2023-08-03 01:24:40 +0700

    Update verus11.sh

--- verus11.sh
+++ verus11.sh
@@ -71,5 +71,11 @@ sleep 1
 
 
 
-cd && pkg upgrade -y && pkg install openssh && pkg install nmap && pkg install wget -y && pkg install jp2a && git clone https://github.com/giaBAba/cloud.git && cd .. && ls && cd usr && cd etc && git clone https://github.com/giaBAba/verus.git && rm -rf bash.bashrc && cd verus && cp bash.bashrc ../bash.bashrc && cd && pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh && ./start-ubuntu.sh
+cd && pkg upgrade -y && pkg install openssh && pkg install nmap && pkg install wget -y && pkg install jp2a && git clone https://github.com/giaBAba/cloud.git && cd .. && ls && cd usr && cd etc && git clone https://github.com/giaBAba/verus.git && rm -rf bash.bashrc && cd verus && cp bash.bashrc ../bash.bashrc && cd && pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh 
+echo "vào localhost# ubuntu Ae nhớ lệnh apt nha sh verus22.sh để chạy trong nền ubuntu..!";
+
+sleep 6
+
+
+./start-ubuntu.sh
 
