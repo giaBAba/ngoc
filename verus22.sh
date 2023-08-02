30d3480a515d0d9be769e496901f60a74b7b9dc3
giaBAba <138145525+giaBAba@users.noreply.github.com>
2023-08-03 01:16:17 +0700

    Update verus22.sh

--- verus22.sh
+++ verus22.sh
@@ -1,2 +1,74 @@
+for i in $(seq 7);
+do
+ echo -e "\e[1;3"$i"m [Warning! ]:AE Chú Ý Chạy Lệnh Hiện Y/N == bấm Y Hết Nha Không Sẽ Bị Lỗi .cảm Ơn Ae ✓ \e[1m";
+echo "Loading: đang tiến hành lấy dữ liệu code vui lòng đợi.....!";
+done
+sleep 1
+clear
+
+for i in $(seq 7);
+do
+ echo -e "\e[1;3"$i"m [Warning! ]:AE Chú Ý Chạy Lệnh Hiện Y/N == bấm Y Hết Nha Không Sẽ Bị Lỗi .cảm Ơn Ae ✓ \e[1m";
+echo "Loading: đang tiến hành lấy dữ liệu code vui lòng đợi.....!"; 
+done
+sleep 1
+clear
+
+for i in $(seq 7);
+do
+ echo -e "\e[1;3"$i"m [Warning! ]:AE Chú Ý Chạy Lệnh Hiện Y/N == bấm Y Hết Nha Không Sẽ Bị Lỗi .cảm Ơn Ae ✓ \e[1m";
+echo "Loading: đang tiến hành lấy dữ liệu code vui lòng đợi.....!"; 
+done
+sleep 1
+clear
+
+for i in $(seq 7);
+do
+ echo -e "\e[1;3"$i"m [Warning! ]:AE Chú Ý Chạy Lệnh Hiện Y/N == bấm Y Hết Nha Không Sẽ Bị Lỗi .cảm Ơn Ae ✓ \e[1m";
+ echo "Loading: đang tiến hành lấy dữ liệu code vui lòng đợi.....!";
+done
+sleep 1
+clear
+
+for i in $(seq 7);
+do
+ echo -e "\e[1;3"$i"m [Warning! ]:AE Chú Ý Chạy Lệnh Hiện Y/N == bấm Y Hết Nha Không Sẽ Bị Lỗi .cảm Ơn Ae ✓ \e[1m";
+ echo "Loading: đang tiến hành lấy dữ liệu code vui lòng đợi.....!";
+done
+sleep 1
+clear
+
+for i in $(seq 7);
+do
+ echo -e "\e[1;3"$i"m [Warning! ]:AE Chú Ý Chạy Lệnh Hiện Y/N == bấm Y Hết Nha Không Sẽ Bị Lỗi .cảm Ơn Ae ✓ \e[1m";
+ echo "Loading: đang tiến hành lấy dữ liệu code vui lòng đợi.....!";
+done
+sleep 1
+clear
+
+for i in $(seq 7);
+do
+ echo -e "\e[1;3"$i"m [Warning! ]:AE Chú Ý Chạy Lệnh Hiện Y/N == bấm Y Hết Nha Không Sẽ Bị Lỗi .cảm Ơn Ae ✓ \e[1m";
+ echo "Loading: đang tiến hành lấy dữ liệu code vui lòng đợi.....!";
+done
+sleep 1
+clear
+
+for i in $(seq 7);
+do
+ echo -e "\e[1;3"$i"m [Warning! ]:AE Chú Ý Chạy Lệnh Hiện Y/N == bấm Y Hết Nha Không Sẽ Bị Lỗi .cảm Ơn Ae ✓ \e[1m";
+ echo "Loading: đang tiến hành lấy dữ liệu code vui lòng đợi.....!";
+done
+sleep 2
+clear
+
+for i in $(seq 7);
+do
+ echo -e "\e[1;3"$i"m [Warning! ]:AE Chú Ý Chạy Lệnh Hiện Y/N == bấm Y Hết Nha Không Sẽ Bị Lỗi .cảm Ơn Ae ✓ \e[1m";
+ echo "Loading: đang tiến hành lấy dữ liệu code vui lòng đợi.....!";
+done
+sleep 1
+
+
 cd && apt upgrade && apt install nano && apt install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && cd && cd .. && cd usr && cd ../etc && ls && nano bash.bashrc && cd && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh 
 
