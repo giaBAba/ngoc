4f8d67345e006c59cd6c4cae271e88c814d1efd4
giaBAba <138145525+giaBAba@users.noreply.github.com>
2023-08-06 09:30:22 +0700

    Update verus22.sh

--- verus22.sh
+++ verus22.sh
@@ -71,4 +71,5 @@ sleep 1
 
 
 cd && apt upgrade && apt install nano && apt install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && cd && cd .. && cd usr && cd ../etc && ls && nano bash.bashrc && cd && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh 
+cd
 echo "diện gửi lời nhắn: Đã cài đặt hoàn tất vui lòng đóng ứng dụng và khỏi động lại để reboot auto...!";
